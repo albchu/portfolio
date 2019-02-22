1a65da3c99652cfbe8004ec1215ca8467de72e06
Albert Chu <albertchu539@gmail.com>
2019-02-22 01:21:14 -0700

    Renamed header dir

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import Header from '../components/Header';
+import Header from '../components/header';
 import PageDownButton from '../components/PageDownButton/PageDownButton';
 
 const IndexPage = () => (
