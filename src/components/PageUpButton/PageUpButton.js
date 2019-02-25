be544eaf353b2fda5cb6df3c2cd27ef8a89c9172
Albert Chu <albertchu539@gmail.com>
2019-02-25 01:24:17 -0700

    Increased scroll up button increment

--- src/components/PageUpButton/PageUpButton.js
+++ src/components/PageUpButton/PageUpButton.js
@@ -8,7 +8,7 @@ const PageUpButton = ({ className, disable }) => {
   return (
     <UpButton
       className={classnames('pageButton', disable && 'disable')}
-      onClick={() => !disable && scroll.scrollMore(780)}
+      onClick={() => !disable && scroll.scrollMore(800)}
     />
   );
 };
