14b9e3feaab1b84a5a04a9985baf272498499104
Albert Chu <albertchu539@gmail.com>
2019-02-28 19:47:03 -0700

    Small tweaks to about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './About.css';
 
 const About = () => {
-  return <div className="About__container">ABOUT MEE</div>;
+  return (
+    <div className="About__container">
+      <h1>ABOUT ME</h1>
+    </div>
+  );
 };
 
 export default About;
