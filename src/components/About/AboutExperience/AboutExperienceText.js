5aafcdef8d75408dde73a5b61b9e73e92fb51f8b
Albert Chu <albertchu539@gmail.com>
2019-03-27 00:21:21 -0600

    Made about experience years count dynamically

--- src/components/About/AboutExperience/AboutExperienceText.js
+++ src/components/About/AboutExperience/AboutExperienceText.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './AboutExperienceText.css';
 
+const diffYears = (dt2, dt1 = new Date()) => {
+  const diffSecs = (dt2.getTime() - dt1.getTime()) / 1000;
+  const diffDays = diffSecs / (60 * 60 * 24);
+
+  return Math.abs(Math.round(diffDays / 365.25));
+};
+
 const AboutExperienceText = () => {
   return (
     <div className="AboutExperienceText__container">
@@ -11,7 +18,9 @@ const AboutExperienceText = () => {
       <div className="AboutExperienceText__Row_3">
         perspective in my career of
       </div>
-      <div className="AboutExperienceText__Row_4">6 years</div>
+      <div className="AboutExperienceText__Row_4">
+        {diffYears(new Date('June 01 2013'))} years
+      </div>
       <div className="AboutExperienceText__Row_5">in software development</div>
     </div>
   );
